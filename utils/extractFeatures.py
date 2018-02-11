933dcbfb82ae29b9390a13922ada091b67c84e8c
Akshay Bhatia <akshaybhatia10@gmail.com>
2018-02-11 18:11:08 +0530

    implemented convert_onehot

---
+++ utils/extractFeatures.py
@@ -0,0 +1,30 @@
+import glob
+import numpy as np
+from preprocessMidi import convert_midi
+
+def convert_onehot(file):
+	new = np.zeros(file.shape)
+	for i in range(len(file)):
+		nz = np.nonzero(file[i])
+		if len(nz[0]) < 0:
+			new[i, nz[0][-1]] = 1
+
+	return new		
+
+def get_songs(dir, min_len=128):
+	files = glob.glob(dir)
+	all_songs = []
+	for file in files:
+		new = convert_midi(file)
+		new = convert_onehot(new)
+		if len(new) > min_len:
+			all_songs.append(new)
+
+	return all_songs		
+
+def main():
+	dir = 'data/Taylor Swift - Shake It Off.mid'
+	print (get_songs(dir))
+
+if __name__ == '__main__':
+	main()	
