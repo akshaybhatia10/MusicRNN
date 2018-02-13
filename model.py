2ec92067556add392a25a60e50e0427b58c36b32
Akshay Bhatia <akshaybhatia10@gmail.com>
2018-02-13 16:14:25 +0530

    implemented get_accuracy function

--- model.py
+++ model.py
@@ -2,18 +2,49 @@ import tensorflow as tf
 import numpy as np
 
 def model_placeholders(input_size, output_size):
-	inputs = tf.placeholder(tf.float32, shape=(None, timestamp, input_size), name='input_placeholder')
-	targets = tf.placeholder(tf.float32, shape=(None, output_size), name='output_placeholder')
-	keep_probability = tf.placeholder(tf.float32, name="keep_prob")
-	learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+	model_inputs = tf.placeholder(tf.float32, shape=(None, timestamp, input_size), name='input_placeholder')
+	model_targets = tf.placeholder(tf.float32, shape=(None, output_size), name='output_placeholder')
+	keep_prob = tf.placeholder(tf.float32, name="keep_prob")
+	lr = tf.placeholder(tf.float32, name='learning_rate')
 
 	return inputs, targets, keep_probability, learning_rate
 
-def model_parameters(hidden_size):
+
+def model_parameters(hidden_size, output_size):
 	w1 = tf.Variable(tf.random_normal([hidden_size, output_size]))
-	b1 = tf.Variable(tf.random_normal([hidden_size, 1]))
+	b1 = tf.Variable(tf.random_normal([output_size]))
 
 	return w1, b1
 
 
+def rnn_layer(inputs, parameters, rnn_units, keep_prob):
+	inputs, tf.unstack(inputs, timestep)
+	w1, b1 = parameters[0], parameters[1]
+	basic_lstm = tf.contrib.rnn.BasicLSTMCell(rnn_units)
+	dropout = tf.contrib.rnn.DropoutWrapper(basic_lstm, keep_prob)
+	outputs, state = tf.contrib.rnn.static_rnn(basic_lstm, inputs, dtype=tf.float32)
 
+	final_outputs = tf.matmul(outputs[-1], w1) + b1
+	pred = tf.nn.softmax(final_outputs)
+
+	return final_outputs, prediction
+
+
+def get_loss(final_outputs, targets):
+	loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits
+						  (logits=final_outputs, labels=targets))		
+
+	return loss
+
+
+def get_optimizer(loss, learning_rate):
+	optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+	return optimizer
+
+
+def get_accuracy(targets, prediction):
+	correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(targets, 1))
+	acc = tf.reduce_mean(tf.cast(correct, tf.float32))
+
+	return acc
